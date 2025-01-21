d56491a18017b80740c599137a3e53880e6af766
kaylammc <kayla_mclaughlin@ucsb.edu>
2025-01-21 09:16:55 -0800

    Prog101

--- exercises/prog101-expressions-variables.R
+++ exercises/prog101-expressions-variables.R
@@ -1,15 +1,20 @@
 # What do the following expressions do?
 temp_F <- temp_C * 9/5 - 32
+# Converting Celsius to Fahrenheit
 rect_area <- rect_length * rect_width
+# Multiplying length by width to get area
 
 # Fill in the question marks to complete these expressions
-????_area <- 1/2 * base * height
-cylinder_volume <- pi * ????^2 * ???
+triangle_area <- 1/2 * base * height
+cylinder_volume <- pi * radius^2 * height
 
-  # What are the errors in these expressions?
-  speed_m_s <- distance_m * time_s
-acceleration m s2 <- speed_m_s / time_s
-force_N < mass + acceleration_m_s2
+# What are the errors in these expressions?
+speed_m_s <- distance_m * time_s
+acceleration_m_s2 <- speed_m_s / time_s
+# The acceleration is put in incorrectly because it's not saying (meters per
+# second squared) it's just an m and an s with a 2 next to it.
+force_N < mass * acceleration_m_s2
+# Force is mass TIMES acceleration, not plus.
 
 # The energy density of market squid is 4850 joules per gram[1]. If a 225 kg
 # Risso's dolphin needs 5 million joules of energy per day[2] and a typical
@@ -17,6 +22,15 @@ force_N < mass + acceleration_m_s2
 # eat each day? Solve the above word problem in R. Consider how many variables
 # you need to create, what to name them to make the code readable, and how to
 # combine them into expressions.
+energy_density_marketsquid_Jg <- 4850
+weight_Rissodolphin_kg <- 225
+energy_Rissodolphin_J <- 5000000
+weight_marketsquid_g <- 35
+# Find energy in one market squid.
+energy_per_squid_J <- energy_density_marketsquid_Jg * weight_marketsquid_g
+# Find amount of squid a dolphin needs to eat per day.
+amount_squid_dolphinneeds <- energy_Rissodolphin_J / energy_per_squid_J
+# A Risso dolphin needs to eat 30 squid per day.
 
 # [1] Price et al. 2024 https://doi.org/10.3389/fmars.2023.1345525
 # [2] Barlow et al. 2008 https://doi.org/10.3354/meps07695
--- prog101.R
+++ prog101.R
@@ -1,5 +1,5 @@
 ##########################################################################
-## Driver: (Name) (GitHub Handle)                                       ##
+## Driver: (Kayla McLaughlin) (kaylammc)                                ##
 ## Navigator: (Name) (GitHub Handle)                                    ##
 ## Date: (YYYY-MM-DD)                                                   ##
 ##########################################################################
@@ -9,73 +9,89 @@ library(marinecs100b)
 # Guiding questions -------------------------------------------------------
 
 # What does KEFJ stand for?
+?kefj
+# kefj stands for Kenai Fjords Temperature Readings
 
 # How was temperature monitored?
+# HOBO V2 temperature loggers were place at each rocky site
 
 # What's the difference between absolute temperature and temperature anomaly?
-
+# Absolute temperature is the recorded temperature by the sensor. Temperature
+# anomaly is the difference between the long-term average temperature and the
+# actual temperature.
 
 # Begin exploring ---------------------------------------------------------
 
 # How many kefj_* vectors are there?
+# 6
 
 # How long are they?
+length(kefj_temperature)
+# 2187966
 
-# What do they represent?
+# What do they represent? Temperature, site name, date and time of reading, tide
+# level, temperature exposure, and season of the year.
 
 # Link to sketch
 
-???_datetime <- kefj_datetime[kefj_site == ???]
-???_interval <- ???_datetime[-1] - ???_datetime[-???]
-t???e(???)
+aialik_datetime <- kefj_datetime[kefj_site == "Aialik"]
+aialik_interval <- aialik_datetime[2:length(aialik_datetime)] - aialik_datetime
+[1:length(aialik_datetime)-1]
+
+table(aialik_interval)
 
 
 # Problem decomposition ---------------------------------------------------
 
 # When and where did the hottest and coldest air temperature readings happen?
+max(kefj_temperature)
+min (kefj_temperature)
+kefj_site[which.max(kefj_temperature)]
+kefj_site[which.min(kefj_temperature)]
+kefj_datetime[which.max(kefj_temperature)]
+kefj_datetime[which.min(kefj_temperature)]
+# The hottest air temperature happened at Aialik on July 3, 2018 at 1pm.
+# The coldest air temperature happened at Aialik on January 27, 2013 at 9pm.
 
 # Link to sketch
 
 # Plot the hottest day
 
-hottest_idx <- ???(kefj_temperature)
-hottest_time <- ???[hottest_idx]
-??? <- kefj_site[???]
-hotday_start <- as.POSIXct("???", tz = "Etc/GMT+8")
-hotday_end <- as.POSIXct("???", tz = "Etc/GMT+8")
-hotday_idx <- ??? == hottest_site &
-  ??? >= hotday_start &
-  ??? <= hotday_end
-hotday_datetime <- ???[hotday_idx]
-hotday_temperature <- ???
-hotday_exposure <- ???
-plot_kefj(???, ???, ???)
+hottest_idx <- which.max(kefj_temperature)
+hottest_time <- kefj_datetime[hottest_idx]
+hottest_site <- kefj_site[hottest_idx]
+hotday_start <- as.POSIXct("2018-07-03 00:00", tz = "Etc/GMT+8")
+hotday_end <- as.POSIXct("2018-07-03 23:59:59", tz = "Etc/GMT+8")
+hotday_idx <- which(kefj_site == hottest_site &
+  kefj_datetime >= hotday_start &
+  kefj_datetime <= hotday_end)
+hotday_datetime <- kefj_datetime[hotday_idx]
+hotday_temperature <- kefj_temperature[hotday_idx]
+hotday_exposure <- kefj_exposure[hotday_idx]
+plot_kefj(hotday_datetime, hotday_temperature, hotday_exposure)
 
 # Repeat for the coldest day
 
+coldest_idx <- which.min(kefj_temperature)
+coldest_time <- kefj_datetime[coldest_idx]
+coldest_site <- kefj_site[coldest_idx]
+coldday_start <- as.POSIXct("2013-01-27 00:00", tz = "Etc/GMT+8")
+coldday_end <- as.POSIXct("2013-01-27 23:59:59", tz = "Etc/GMT+8")
+coldday_idx <- which(kefj_site == coldest_site &
+  kefj_datetime >= coldday_start &
+  kefj_datetime <= coldday_end)
+coldday_datetime <- kefj_datetime[coldday_idx]
+coldday_temperature <- kefj_temperature[coldday_idx]
+coldday_exposure <- kefj_exposure[coldday_idx]
+plot_kefj(coldday_datetime, coldday_temperature, coldday_exposure)
+
 # What patterns do you notice in time, temperature, and exposure? Do those
-# patterns match your intuition, or do they differ?
+# patterns match your intuition, or do they differ? For the cold, temperature is
+# stable then drastically decreases at night where there is zero sun exposure.
+# For the hot, there is a peak temperature in the middle of the day, where there
+# is the highest exposure to sunlight.
 
 # How did Traiger et al. define extreme temperature exposure?
-
-# Translate their written description to code and calculate the extreme heat
-# exposure for the hottest day.
-
-# Compare your answer to the visualization you made. Does it look right to you?
-
-# Repeat this process for extreme cold exposure on the coldest day.
+# He defined it as heatwaves.
 
 
-# Putting it together -----------------------------------------------------
-
-# Link to sketch
-
-# Pick one site and one season. What were the extreme heat and cold exposure at
-# that site in that season?
-
-# Repeat for a different site and a different season.
-
-# Optional extension: Traiger et al. (2022) also calculated water temperature
-# anomalies. Consider how you could do that. Make a sketch showing which vectors
-# you would need and how you would use them. Write code to get the temperature
-# anomalies for one site in one season in one year.
